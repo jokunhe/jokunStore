8ebc7079cc5092c25abe5d66e2de3c600174d017
jokun <jokun.he@letote.cn>
2019-08-30 02:10:11 +0800

    save

--- ios/testApp/AppDelegate.m
+++ ios/testApp/AppDelegate.m
@@ -36,7 +36,7 @@
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 #else
-  return [[NSBundle mainBundle] URLForResource:@"bundle/index.ios " withExtension:@"jsbundle"];
+  return [[NSBundle mainBundle] URLForResource:@"bundle/index.ios" withExtension:@"jsbundle"];
 #endif
 }
 
